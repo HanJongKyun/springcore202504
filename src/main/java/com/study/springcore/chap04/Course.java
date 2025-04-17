1551dfedb37f996401d03aed5120d84f14045f8b
Jongkyun <hjk326333@gmail.com>
2025-04-17 16:09:53 +0900

    Spring core 모듈 의존성 관리 학습 중(chap04)

---
+++ src/main/java/com/study/springcore/chap04/Course.java
@@ -0,0 +1,6 @@
+package com.study.springcore.chap04;
+
+public interface Course {
+
+    void combineMenu();
+}
--- src/test/java/com/study/springcore/HotelTest.java
+++ src/test/java/com/study/springcore/HotelTest.java
@@ -1,29 +1,21 @@
 package com.study.springcore;
 
-/**
- * @solution
- * - 객체 생성의 제어권을 이 클래스에서
- *   다른 클래스로 이전한다. (AppConfig)
- *   ex) new 생성자(); -> 이 문법을 담당 클래스를 정해서 몰아서 수행시킴
- *   만약 필요로 하는 의존객체가 있다면 미리 주입을 시켜놓는 로직까지 작성.
- *
- *   제어의 역전(IoC): 객체 생성의 제어권을 외부에서 처리하는 것.
- *   의존성 주입(DI): 외부에서 생성된 객체를 주입받는 개념.
- *
- * @problem - 추상화를 했지만 여전히 의존 객체를 바꾸려면
- *            코드를 직접 변경해야 한다.
- */
-
-import com.study.springcore.chap03.Hotel;
-import com.study.springcore.chap03.config.AppConfig;
+import com.study.springcore.chap04.Hotel;
+import com.study.springcore.chap04.config.AppConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class HotelTest {
 
     @Test
     void hotelTest() {
-        AppConfig config = new AppConfig();
-        Hotel hotel = config.hotel();
+        // 스프링 컨테이너 (등록한 빈(객체)을 담아놓는 컨테이너)를 로딩하는 객체를 생성
+        // 설정파일 (Appconfig) 클래스에 지정한 형식을 사용
+        AnnotationConfigApplicationContext ctx
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Hotel 클래스 타입으로 등록된 bean을 리턴받는 메서드
+        Hotel hotel = ctx.getBean(Hotel.class);
 
         hotel.inform();
     }
